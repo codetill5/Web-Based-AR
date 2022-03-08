58ff065cb0eae71ef6c5f62ee80da04e8ae9f64d
shubham <mehrashubham216@gmail.com>
2022-03-08 12:37:26 +0530

    uri change

--- components/Model.js
+++ components/Model.js
@@ -3,9 +3,9 @@ import '@google/model-viewer';
 const Model = () => (
   <div id="card">
   <model-viewer
-    src="/gladiador.glb"
+    src="https://www.arweave.net/hZIaHXCq7UcMvevRZ0VtMlMxPnfVS9ZYizoGYjZ_WlE?ext=glb"
     ios-src=""
-    poster="/auctions.jpg"
+    poster="https://www.arweave.net/vyzBWnC5lJql4UISImR8LS32TBCwN_SIiL0W1WPgRec?ext=PNG"
     alt="A 3D model of an astronaut"
     shadow-intensity="1"
     camera-controls
