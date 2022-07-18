165b077963c940b3c47420fe4fde977d6a806be9
codetill5 <mehrashubham216@gmail.com>
2022-07-18 20:02:11 +0530

    changed glb

--- components/Model.js
+++ components/Model.js
@@ -3,18 +3,18 @@ import '@google/model-viewer';
 const Model = () => (
   <div id="card">
     <model-viewer
-      src="https://ipfs.io/ipfs/bafybeigmrbo7mczg2w2szxdzowqm4w6zetk6cps5jtwbw4gx3zs3xmx364"
+      src="/Bear_Walk.glb"
       ios-src=""
-      poster="https://ipfs.io/ipfs/bafkreigigidy73q2c6jnjakmy4b62xadmk22xcnl4dwmbq7n2ih2tuj4su"
-      alt="A 3D model of an astronaut"
+      poster="https://ichef.bbci.co.uk/news/976/cpsprodpb/EF37/production/_124293216_gettyimages-1237158373.jpg"
+      alt="A 3D model of an bear"
       shadow-intensity="1"
       camera-controls
       auto-rotate
       ar
     >
-      <div id="annotation" style={{ height: '100%', width: '100px', display: 'flex', justifyContent: 'flex-end', alignItems: 'end' }}>
+      {/* <div id="annotation" style={{ height: '100%', width: '100px', display: 'flex', justifyContent: 'flex-end', alignItems: 'end' }}>
         <h1>Hello world</h1>
-      </div>
+      </div> */}
       {/* <img src='/logo-big.png' style={{ margin: '10px -8px', width: '100%' }} /> */}
     </model-viewer>
   </div>
